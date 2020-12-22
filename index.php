9b1136c3339dc5a0051f77a4fe51544c7bbdbaff
rokydas <droky136@gmail.com>
2020-12-22 22:37:35 +0600

    some design added

--- index.php
+++ index.php
@@ -4,9 +4,51 @@
     <meta charset="utf-8">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1 class="text-center">This is a employee management system</h1>
+    <h1 class="text-center m-5">This is an employee management system</h1>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="box mb-5">
+            <h3>Insert Employee</h1>
+          </div>
+          <form class="" action="" method="post">
+            <input class="form-control" type="text" name="name" placeholder="Enter your Name:"><br>
+            <input class="form-control" type="text" name="email" placeholder="Enter your Email:"><br>
+          </form>
+        </div>
+        <div class="col-md-8">
+          <div class="box mb-5">
+            <h3>List of employees</h1>
+          </div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Hobbies</th>
+                <th>Age</th>
+                <th>Date of Birth</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Roky Das</td>
+                <td>droky1s6@gmail.com</td>
+                <td>Male</td>
+                <td>Gardening</td>
+                <td>22</td>
+                <td>19<sup>th</sup> July, 1998</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
 
 
 
